b76a57d18d1756e5bf22ee07d2d4582ba70628b5
George Barh <georgios.barchampas@students.codeinstitute.net>
2025-03-14 15:57:08 +0000

    add view html page

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,7 @@ class Post(models.Model) :
     excerpt = models.TextField(blank= True)
     updated_on = models.DateTimeField(auto_now = True)
 
+
     class Meta:
         ordering = ["-created_on"]
     
@@ -30,6 +31,9 @@ class Comment(models.Model) :
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    challenge = models.FloatField(default = 3.0 )
+
+
 
     class Meta: 
         ordering= ["created_on"]
@@ -38,8 +42,7 @@ class Comment(models.Model) :
         return f"Comment: {self.body} by {self.author}"
 
 
-        
-
+    
 
 
 
