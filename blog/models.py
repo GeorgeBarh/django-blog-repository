58f3bd7fe81d29721988b4a3b5bfdb72a9135ca7
George Barh <georgios.barchampas@students.codeinstitute.net>
2025-03-16 15:44:44 +0000

    urls changes

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import models to connect
 
-
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-
-STATUS=((0,"Draft"), (1, "Published"))
-
-class Post(models.Model) : 
-    title = models.CharField(max_length=200, unique= True)
-    slug = models.SlugField(max_length=200,unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name="blog_posts")
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS,default=0)
-    excerpt = models.TextField(blank= True)
-    updated_on = models.DateTimeField(auto_now = True)
-
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -25,26 +23,18 @@ class Post(models.Model) :
         return f"{self.title} | written by {self.author}"
 
 
-class Comment(models.Model) :
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name="commenter")
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    challenge = models.FloatField(default = 3.0 )
+    new_field = models.SlugField()
 
-
-
-    class Meta: 
-        ordering= ["created_on"]
+    class Meta:
+        ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment: {self.body} by {self.author}"
-
-
-    
-
-
-
-
-
+        return f"Comment {self.body} by {self.author}"
