5a8a27702ec872244e848df32c598776acdf1d19
George Barh <georgios.barchampas@students.codeinstitute.net>
2025-04-04 10:08:06 +0000

    load crispy forms in about.html

--- about/views.py
+++ about/views.py
@@ -4,17 +4,14 @@ from .forms import CollaborateForm
 
 
 def about_me(request):
-    """
-    Renders the About page
-    """
     about = About.objects.all().order_by('-updated_on').first()
-
     collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about,
-         "collaborate_form" : collaborate_form,
-         },
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
     )
